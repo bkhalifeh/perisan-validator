f40e34185f6507062eb5659a051bb3c7754580f5
wexsilon <wexsilon2001@protonmail.com>
2023-12-13 20:20:18 +0330

    implmenet IsIranianIdentityCard

--- index.ts
+++ index.ts
@@ -1,3 +1,21 @@
-export function IsIranianIdentityCard(nid: string) {
-    return true;
+import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator';
+
+export function IsIranianIdentityCard(property: string, validationOptions?: ValidationOptions) {
+    return function (object: Object, propertyName: string) {
+        registerDecorator({
+            name: 'isIranianIdentityCard',
+            target: object.constructor,
+            propertyName: propertyName,
+            constraints: [property],
+            options: validationOptions,
+            validator: {
+                validate(value: any, args: ValidationArguments) {
+                    if (!/^\d{10}$/.test(value)) return false;
+                    const check = +value[9];
+                    const sum = value.split('').slice(0, 9).reduce((acc, x, i) => acc + +x * (10 - i), 0) % 11;
+                    return sum < 2 ? check === sum : check + sum === 11;
+                },
+            },
+        });
+    };
 }
