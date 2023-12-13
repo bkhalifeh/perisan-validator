c457b0117fc5e790184260667b2cf699286c0af3
wexsilon <wexsilon2001@protonmail.com>
2023-12-13 20:25:46 +0330

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,2 @@
-export declare function IsIranianIdentityCard(nid: string): boolean;
+import { ValidationOptions } from 'class-validator';
+export declare function IsIranianIdentityCard(property: string, validationOptions?: ValidationOptions): (object: Object, propertyName: string) => void;
--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.IsIranianIdentityCard = void 0;
-function IsIranianIdentityCard(nid) {
-    return true;
+const class_validator_1 = require("class-validator");
+function IsIranianIdentityCard(property, validationOptions) {
+    return function (object, propertyName) {
+        (0, class_validator_1.registerDecorator)({
+            name: 'isIranianIdentityCard',
+            target: object.constructor,
+            propertyName: propertyName,
+            constraints: [property],
+            options: validationOptions,
+            validator: {
+                validate(value, args) {
+                    if (!/^\d{10}$/.test(value))
+                        return false;
+                    const check = +value[9];
+                    const sum = value.split('').slice(0, 9).reduce((acc, x, i) => acc + +x * (10 - i), 0) % 11;
+                    return sum < 2 ? check === sum : check + sum === 11;
+                },
+            },
+        });
+    };
 }
 exports.IsIranianIdentityCard = IsIranianIdentityCard;
 //# sourceMappingURL=index.js.map
