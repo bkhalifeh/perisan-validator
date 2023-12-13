f4858f2958a8772cba54c1a12cccdc8bb27351f3
wexsilon <wexsilon2001@protonmail.com>
2023-12-13 19:57:16 +0330

    build

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export declare function IsIranianIdentityCard(nid: string): boolean;
---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.IsIranianIdentityCard = void 0;
+function IsIranianIdentityCard(nid) {
+    return true;
+}
+exports.IsIranianIdentityCard = IsIranianIdentityCard;
+//# sourceMappingURL=index.js.map
